673785e896c6e4553619970ebe5f370ce8dd7a64
RieSon <8330186+Rieson1@user.noreply.gitee.com>
2021-02-16 14:41:09 +0800

    添加文件

---
+++ src/main/java/com/pojo/test/Hello.java
@@ -0,0 +1,11 @@
+package com.pojo.test;
+
+/**
+ * @author rieson
+ * @create 2021-02-16-14:28
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("一帆风顺");
+    }
+}
---
+++ src/main/java/com/pojo/test/Hello1.java
@@ -0,0 +1,11 @@
+package com.pojo.test;
+
+/**
+ * @author rieson
+ * @create 2021-02-16-14:37
+ */
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
