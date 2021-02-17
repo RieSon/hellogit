1b65e1ac2afb8e376add14ee7e3eeb3c757e2bf3
RieSon <8330186+Rieson1@user.noreply.gitee.com>
2021-02-17 09:54:16 +0800

    主线添加

--- src/main/java/com/pojo/test/Hello.java
+++ src/main/java/com/pojo/test/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("一帆风顺");
         System.out.println("创建分支");
+        System.out.println("主线添加");
     }
 }
